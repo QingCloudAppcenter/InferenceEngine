8fdc0fd1b80495997ec174632182d0d23a7f2b3d
Benjamin <benjamin@yunify.com>
2019-08-13 15:41:12 +0800

    first commit

---
+++ tensorflow_serving/README.md
@@ -0,0 +1,6 @@
+<p align="center">
+  <img src="./logo.png" alt="Logo" />
+</p>
+<h1 align="center">ELK on QingCloud</h1>
+
+- [Product Page](https://appcenter.qingcloud.com/apps/app-p6au3oyq)
---
+++ tensorflow_serving/ansible/roles/model_repo/files/opt/app/bin/role.sh
@@ -0,0 +1,38 @@
+host="localhost"
+port="9000"
+DOCKER_HOME=/usr/local/bin
+
+prepareDir() {
+  mkdir -p /data/minio/{logs,data} && chgrp -R syslog /data/minio/logs && chmod -R 775 /data && mkdir -p /data/models_to_load && mv /opt/models /data/minio/data/models
+}
+
+init() {
+  _init
+  prepareDir
+#  local htmlPath=/data/elasticsearch/index.html
+#  [ -e $htmlPath ] || ln -s /opt/app/conf/caddy/index.html $htmlPath
+}
+
+start () {
+  cd /opt/app/bin && $DOCKER_HOME/docker-compose up -d
+}
+
+stop () {
+  cd /opt/app/bin && $DOCKER_HOME/docker-compose down
+}
+
+restart() {
+  cd /opt/app/bin && $DOCKER_HOME/docker-compose down && $DOCKER_HOME/docker-compose up -d
+}
+
+check() {
+  nc -z -w5 $host $port
+}
+
+revive() {
+  restart
+}
+
+update() {
+  return 0
+}
